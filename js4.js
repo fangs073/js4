659331b1f10b8879300f44c3268ba5b5aed96b48
Student <student@RU-003.local>
2016-01-18 14:32:59 -0700

    Completed

--- js4.js
+++ js4.js
@@ -15,13 +15,25 @@ function getInfo (form, destinationArray) {
 	destinationArray.push(out);
 }
 
+function addRow(tableID, srcArr) {
+	var table = document.getElementById(tableID);
+	var row = table.insertRow(srcArr.length+1);
+	var obj = srcArr[srcArr.length-1];
+	var name = row.insertCell(0);
+	var phone = row.insertCell(1);
+	var street = row.insertCell(2);
+	name.innerHTML = obj.name;
+	phone.innerHTML = obj.phone;
+	street.innerHTML = obj.street;
+}
+
 function createOutput (srcArr) {
 	var out = [];
 	var keys = Object.keys(srcArr[0]);
 	for (var i = srcArr.length - 1; i >= 0; i--) {
 		for (var k = 0; k < keys.length; k++) {
 			out.push(keys[k] + " : " + srcArr[i][keys[k]]);
-		}
+		};
 	};
 
 	return out.join("\n");
